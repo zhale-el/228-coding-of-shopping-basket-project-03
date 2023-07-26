7c3c68898117dd175ddb01a638b14b355059b4b3
zhaleh <zhalehelmi@gmail.com>
2023-07-26 16:32:39 +0330

    calcTotal

--- js/store.js
+++ js/store.js
@@ -1,15 +1,28 @@
 let allProducts = [
-  { id: 1, title: "Album 1", price: 12.93, img: "Images/Album 1.png" },
-  { id: 2, title: "Album 2", price: 21, img: "Images/Album 2.png" },
-  { id: 3, title: "Album 3", price: 33, img: "Images/Album 3.png" },
-  { id: 4, title: "Album 4", price: 41.98, img: "Images/Album 4.png" },
-  { id: 5, title: "Coffee", price: 98, img: "Images/Cofee.png" },
-  { id: 6, title: "Shirt", price: 65.33, img: "Images/Shirt.png" },
+  {
+    id: 1,
+    title: "Album 1",
+    price: 12.93,
+    img: "Images/Album 1.png",
+    count: 1,
+  },
+  { id: 2, title: "Album 2", price: 21, img: "Images/Album 2.png", count: 1 },
+  { id: 3, title: "Album 3", price: 33, img: "Images/Album 3.png", count: 1 },
+  {
+    id: 4,
+    title: "Album 4",
+    price: 41.98,
+    img: "Images/Album 4.png",
+    count: 1,
+  },
+  { id: 5, title: "Coffee", price: 98, img: "Images/Cofee.png", count: 1 },
+  { id: 6, title: "Shirt", price: 65.33, img: "Images/Shirt.png", count: 1 },
 ];
 
 const shopItemsContainer = document.querySelector(".shop-items");
 const basketProductsContainer = document.querySelector(".cart-items");
 const removeAllProductsBtn = document.querySelector("#remove-all-product");
+const cartTotalPriceElem = document.querySelector(".cart-total-price");
 let userBasket = [];
 
 allProducts.forEach(function (product) {
@@ -54,7 +67,8 @@ function addProductBasketArray(productId) {
 
   userBasket.push(mainProduct);
   basketProductsGenerator(userBasket);
-  console.log(userBasket);
+
+  calcTotalPrice(userBasket);
 }
 function basketProductsGenerator(userBasketArray) {
   basketProductsContainer.innerHTML = "";
@@ -120,3 +134,10 @@ removeAllProductsBtn.addEventListener("click", function () {
   userBasket = [];
   basketProductsGenerator(userBasket);
 });
+function calcTotalPrice(userBasketArray) {
+  let totalPriceValue = 0;
+  userBasketArray.forEach(function (product) {
+    totalPriceValue += product.count * product.price;
+    cartTotalPriceElem.innerHTML = totalPriceValue;
+  });
+}
